d4800b90ff08a9960c0caa691cae363905b924bf
andojianurag <andojianurag@utexas.edu>
2016-02-14 23:33:56 -0600

    k

--- src/guestbook/PeriodicEmail.java
+++ src/guestbook/PeriodicEmail.java
@@ -5,6 +5,7 @@ import static com.googlecode.objectify.ObjectifyService.ofy;
 import java.io.IOException;
 import java.util.Date;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -32,5 +33,12 @@ public class PeriodicEmail extends HttpServlet{
     
 	resp.sendRedirect("/ofyguestbookConcise.jsp");
     }
+	
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp)
+
+            throws ServletException, IOException {
+		doGet(req, resp);
+	}
 
 }
